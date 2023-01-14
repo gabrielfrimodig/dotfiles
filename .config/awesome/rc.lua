b538aa409384a4e54bb853aea22cd36ac1751965
Gabriel Frimodig <gabrielfrimodig@gmail.com>
2023-01-14 14:15:37 +0100

    awful init

---
+++ .config/awesome/rc.lua
@@ -0,0 +1,42 @@
+-- If LuaRocks is installed, make sure that packages installed through it are
+-- found (e.g. lgi). If LuaRocks is not installed, do nothing.
+pcall(require, "luarocks.loader")
+
+-- Enable hotkeys help widget for VIM and other apps
+-- when client with a matching name is opened:
+require("awful.hotkeys_popup.keys")
+require("awful.autofocus")
+
+-- Awesome library
+local awful = require("awful")
+local gears = require("gears")
+local beautiful = require("beautiful")
+
+-- Widget and layout library
+local wibox = require("wibox")
+
+-- Theme handling library
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/theme.lua")
+
+-- This is used later as the default terminal and editor to run.
+terminal = "kitty"
+editor = os.getenv("EDITOR") or "vim"
+editor_cmd = terminal .. " -e " .. editor
+
+-- Default modkey.
+-- Modkey: Mod4 (Super key) or Mod1 (Alt key) 
+modkey = "Mod4"
+
+require 'bindings'
+
+require 'signals'
+
+require 'config'
+
+require 'rules'
+
+require 'ui'
+
+-- Autostart
+awful.spawn.with_shell("picom")
+awful.spawn.with_shell("feh --bg-scale ~/Pictures/Wallpaper/wallpaper.jpg")
