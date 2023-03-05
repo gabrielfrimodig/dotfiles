169f0832a9a815629b874ba3dd9ce9ce9e87f123
Gabriel Frimodig <gabrielfrimodig@gmail.com>
2023-03-05 18:06:46 +0100

    Volume bar added

--- .config/awesome/ui/bar/widgets/volume.lua
+++ .config/awesome/ui/bar/widgets/volume.lua
@@ -1,7 +1,7 @@
-
 -- Required libraries
 local awful = require("awful")
 local wibox = require("wibox")
+local gears = require("gears")
 local beautiful = require("beautiful")
 local dpi = require("beautiful").xresources.apply_dpi
 local watch = require("awful.widget.watch")
@@ -9,40 +9,94 @@ local watch = require("awful.widget.watch")
 local volume = wibox.widget.textbox()
 volume.font = "JetBrains Mono 10"
 
--- Create a variable to hold the volume icon
-local volume_icon = wibox.widget.textbox()
+local volume_slider = wibox.widget {
+    {
+        id = "volume_slider",
+        widget = wibox.widget.slider,
+        bar_shape = gears.shape.rounded_rect,
+        bar_height = dpi(2),
+        bar_color = "#f5c2e7",
+        handle_color = "#f5c2e7",
+        handle_shape = gears.shape.circle,
+        handle_width = dpi(10),
+        handle_border_color = "#f5c2e7",
+        handle_border_width = dpi(1),
+        maximum = 100,
+        minimum = 0,
+        value = 50,
+        forced_height = dpi(10),
+        forced_width = dpi(100),
+        widget = wibox.widget.slider,
+    },
+    margins = dpi(5),
+    visible = false,
+    widget = wibox.container.margin,
+}
 
--- Define the font and color for the volume icon
-volume_icon.font = "JetBrains Mono 18"
-volume_icon.color = "#f5c2e7"
+-- Function to update the volume level based on the slider value
+local function update_volume(slider_value)
+    awful.spawn('pactl set-sink-volume 0 ' .. slider_value .. '%')
+    volume.text = slider_value .. "%"
+end
 
--- Set the initial icon to the volume up icon
-volume_icon.markup = '<span font="' .. "JetBrains Mono 18" .. '"foreground="'.. "#f5c2e7" ..'">墳 </span>'
-
-watch([[bash -c "amixer get Master | grep 'Left:' | awk -F'[][]' '{ print $2 }'"]], 1, function(_, stdout)
-    volume.text = stdout
-    collectgarbage("collect")
+-- Connect signals to update the volume level with the slider and keyboard
+volume_slider.volume_slider:connect_signal("property::value", function()
+    local slider_value = volume_slider.volume_slider:get_value()
+    update_volume(slider_value)
 end)
 
--- Watch for changes to the mute status
-watch([[bash -c "amixer get Master | grep 'Left:' | awk -F'[][]' '{ print $4 }'"]], 1, function(_, stdout)
-    -- If the volume is muted, set the icon to the volume off icon
-    if string.match(stdout, "off") then
-        volume_icon.markup = '<span font="' .. "JetBrains Mono 18" .. '"foreground="'.. "#f5c2e7" ..'">婢 </span>'
-    -- Otherwise, set the icon to the volume up icon
-    else
-        volume_icon.markup = '<span font="' .. "JetBrains Mono 18" .. '"foreground="'.. "#f5c2e7" ..'">墳 </span>'
+local volume_icon = wibox.widget {
+    markup = '<span font="JetBrains Mono 18" foreground="#f5c2e7">墳 </span>',
+    widget = wibox.widget.textbox,
+}
+
+local volume_widget = wibox.widget {
+    {
+        layout = wibox.layout.fixed.horizontal,
+        volume_icon,
+        volume_slider,
+        wibox.widget{
+            volume,
+            fg = "#f5c2e7",
+            widget = wibox.container.background
+        },
+    },
+    spacing = dpi(1),
+    layout = wibox.layout.fixed.horizontal,
+}
+
+volume_widget:connect_signal("mouse::enter", function()
+    volume_slider.visible = true
+end)
+
+volume_widget:connect_signal("mouse::leave", function()
+    volume_slider.visible = false
+end)
+
+-- Toggle the volume when widget is clicked
+volume:connect_signal("button::press", function(_, _, _, button)
+    if (button == 1) then
+        awful.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")
     end
 end)
 
-return wibox.widget {
-    volume_icon,
-    wibox.widget{
-        volume, 
-        fg = "#f5c2e7",
-        widget = wibox.container.background
-    },
-    spacing = dpi(2),
-    layout = wibox.layout.fixed.horizontal
-}
+watch([[bash -c "amixer get Master | grep -E '(Left|Mono):' | awk -F'[][]' '{print $2, $4}'"]], 1, function(_, stdout)
+    local volume_level, is_muted = string.match(stdout, "(%d+)%% (%a+)")
+    
+    -- Update the volume text
+    volume.text = volume_level .. '%'
+    
+    -- Update the volume slider
+    volume_slider.volume_slider:set_value(tonumber(volume_level))
+    
+    -- Update the volume icon
+    if is_muted == "off" then
+        volume_icon.markup = '<span font="JetBrains Mono 18" foreground="#f5c2e7">婢 </span>'
+    else
+        volume_icon.markup = '<span font="JetBrains Mono 18" foreground="#f5c2e7">墳 </span>'
+    end
+    
+    collectgarbage("collect")
+end)
 
+return volume_widget
