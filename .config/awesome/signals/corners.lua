f2bca543c82a3c6bd899ddbd199b602d127d29a6
gabrielfrimodig <gabrielfrimodig@gmail.com>
2023-11-28 09:16:20 +0100

    Improved handling for fullscreen windows

--- .config/awesome/signals/corners.lua
+++ .config/awesome/signals/corners.lua
@@ -1,15 +1,13 @@
-
 -- Required libraries
 local gears = require("gears")
-local awful = require("awful")
-local wibox = require("wibox")
-local beautiful = require("beautiful")
 
 -- Rounded corners
--- Doesn't work perfectly on fullscreen
-client.connect_signal("manage", function (c)
-    c.shape = function(cr,w,h)
-        gears.shape.rounded_rect(cr,w,h,12)
+client.connect_signal("manage", function(c)
+    c.shape = function(cr, w, h)
+        if not c.fullscreen then
+            gears.shape.rounded_rect(cr, w, h, 12)
+        else
+            gears.shape.rectangle(cr, w, h)
+        end
     end
 end)
-
