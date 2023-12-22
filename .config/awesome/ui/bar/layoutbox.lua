91370f9ab543c41ab51244fe7ef1e94e61d310e4
gabrielfrimodig <gabrielfrimodig@gmail.com>
2023-12-22 22:41:08 +0100

    comment

--- .config/awesome/ui/bar/layoutbox.lua
+++ .config/awesome/ui/bar/layoutbox.lua
@@ -4,6 +4,14 @@ local wibox = require("wibox")
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
+--- Function to create a layoutbox widget for each screen in AwesomeWM.
+-- A layoutbox widget is used to display and control the current layout of each screen.
+-- Users can change the layout by clicking on the layoutbox widget.
+-- Left-click (button 1) cycles through the available layouts in a forward direction.
+-- Right-click (button 3) cycles through the layouts in a backward direction.
+-- Scroll wheel up (button 4) and down (button 5) are also used for cycling through layouts.
+-- @param s The screen for which the layoutbox is being created.
+-- @return A layoutbox widget with margins set and configured for the given screen.
 return function(s)
     -- Create a layoutbox widget
     local layoutbox = awful.widget.layoutbox {
@@ -16,5 +24,6 @@ return function(s)
         }
     }
     layoutbox = wibox.container.margin(layoutbox, dpi(4), dpi(4), dpi(4), dpi(4))
+
     return layoutbox
 end
