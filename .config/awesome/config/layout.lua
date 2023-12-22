7a009e4825177a16a9c02b154880fd665abece9c
gabrielfrimodig <gabrielfrimodig@gmail.com>
2023-12-22 01:32:33 +0100

    useful comment

--- .config/awesome/config/layout.lua
+++ .config/awesome/config/layout.lua
@@ -1,14 +1,15 @@
 -- Required libraries
 local awful = require("awful")
 
+-- Comment or uncommen for layouts of your choice.
 awful.layout.layouts = {
     awful.layout.suit.tile.left,
     awful.layout.suit.floating,
     awful.layout.suit.fair,
     awful.layout.suit.spiral.dwindle,
     awful.layout.suit.magnifier,
-    --awful.layout.suit.tile,
-    --awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
     --awful.layout.suit.fair.horizontal,
     --awful.layout.suit.spiral,
