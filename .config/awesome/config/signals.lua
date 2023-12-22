389c2e73373de5c2304cc5522d3f5721a80b4e6e
gabrielfrimodig <gabrielfrimodig@gmail.com>
2023-12-22 01:28:14 +0100

    removed border_color update

--- .config/awesome/config/signals.lua
+++ .config/awesome/config/signals.lua
@@ -1,6 +1,5 @@
 -- Required libraries
 local awful = require("awful")
-local beautiful = require("beautiful")
 
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function(c)
@@ -15,5 +14,6 @@ client.connect_signal("manage", function(c)
   end
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+-- Optional usage
+-- client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+-- client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
